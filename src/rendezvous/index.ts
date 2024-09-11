073dae1a6c8ca56cbc0981f8fe9fe8a98950c232
joseaurorah <jorora@sharkswebservices.com>
2024-09-11 19:15:12 +0300

    Initial commit

---
+++ src/rendezvous/index.ts
@@ -0,0 +1,29 @@
+/*
+Copyright 2022 The Matrix.org Foundation C.I.C.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/**
+ * @deprecated in favour of MSC4108-based implementation
+ */
+export * from "./MSC3906Rendezvous.ts";
+export * from "./MSC4108SignInWithQR.ts";
+export * from "./RendezvousChannel.ts";
+export * from "./RendezvousCode.ts";
+export * from "./RendezvousError.ts";
+export * from "./RendezvousFailureReason.ts";
+export * from "./RendezvousIntent.ts";
+export * from "./RendezvousTransport.ts";
+export * from "./transports/index.ts";
+export * from "./channels/index.ts";
